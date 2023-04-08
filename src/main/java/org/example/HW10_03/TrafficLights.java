8da3a6089afaa675e840048a13aec4d39bca8b82
Virchi007 <virchi007@hotmail.com>
2023-04-07 19:05:23 -0700

    Задачки

--- src/main/java/org/example/HW10_03/TrafficLights.java
+++ src/main/java/org/example/HW10_03/TrafficLights.java
@@ -2,6 +2,7 @@ package org.example.HW10_03;
 
 public class TrafficLights {
     public static String updateLight(String current) {
+        // //#1
 //        if (current == "red") {
 //            return "green";
 //        } else if (current == "green") {
@@ -9,6 +10,16 @@ public class TrafficLights {
 //        } else
 //
 //            return "red";
+        // //#2
+        //        if (current.equals("green")){
+//            return "yellow";
+//        }
+//        else if (current.equals("yellow")){
+//            return "red";
+//        }
+//        else return "green";
+
+        // //#3
         return (current.equals("red") ? "green" : current.equals("yellow") ? "red" : "yellow");
 
     }
